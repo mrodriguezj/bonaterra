72fb560103895f10d5ece8735899c6037c875900
Misael Rodriguez <rodriguezjmisael@gmail.com>
2025-01-19 18:16:04 -0500

    Prueba de conexion safitsfactoria

--- test/conexionDB/index.php
+++ test/conexionDB/index.php
@@ -1,6 +1,6 @@
 <?php
 // Cargar la configuración desde el archivo config.php
-$config = require __DIR__ . '/config.php';
+$config = require __DIR__ . '/../../config/config.php';
 
 // Crear la conexión utilizando mysqli
 $conn = new mysqli(
